accf18c226800d19df1b6b12dd0795cbf9e8bcc8
joetamburro <joetamburro38@gmail.com>
2016-08-10 10:01:16 -0400

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,27 @@
 
 $(document).ready(function(){
   window.sr = ScrollReveal();
-  sr.reveal('.header .container', {
+  sr.reveal('.cloud-section .container .cloud', {
+    delay: 500,
+    viewFactor: 0.5,
     origin: 'top',
-    duration: 1000,
+    distance: '0px',
+    duration: 1500,
   });
+  sr.reveal('.ip-group', {
+    duration: 2000,
+    scale: 0.5,
+  }, 200);
+  sr.reveal('.crm-section .crm-box', {
+    delay: 200,
+    duration: 1000,
+  }, 500);
+  sr.reveal('.learn-more h4', {
+    delay: 100,
+    duration: 1000,
+  }, 500);
+  sr.reveal('.learn-more .button', {
+    delay: 700,
+    duration: 1000,
+  }, 500);
 })
